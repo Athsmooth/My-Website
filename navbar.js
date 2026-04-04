b97abb81033308c07f705cd5d80689c704ff99f2
Athsmooth <athsmooth@gmail.com>
2026-04-04 16:38:39 -0500

    Updated theming, still no content :(

--- navbar.js
+++ navbar.js
@@ -6,18 +6,5 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
 
     const topnav = document.querySelector('.topnav');
-    
-    if (topnav) {
-        topnav.innerHTML = navHTML;
-
-        // Auto-highlight the active link
-        const currentPath = window.location.pathname.split("/").pop() || "index.html";
-        document.querySelectorAll('.topnav a').forEach(link => {
-            if (link.getAttribute('href') === currentPath) {
-                link.classList.add('active');
-            }
-        });
-    } else {
-        console.error("Could not find the .topnav div!");
-    }
+    topnav.innerHTML = navHTML;
 });
